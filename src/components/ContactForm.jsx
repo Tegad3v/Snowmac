247af221b3aaf0cbd3a65fcbb8067a26b62bd2c2
Okandeji John <johnokandejio@gmail.com>
2026-09-18 12:21:57 +0100

    update

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -31,19 +31,6 @@ function ContactForm() {
   const onSubmit = async (data) => {
     setIsSending(true);
 
-    console.log(
-      import.meta.env.VITE_EMAILJS_SERVICE_ID,
-      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-      {
-        fullName: data.fullName,
-        phone: data.phone,
-        email: data.email,
-        projectType: data.projectType,
-        projectDetails: data.projectDetails,
-      },
-      { publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY },
-    );
-
     try {
       await emailjs.send(
         import.meta.env.VITE_EMAILJS_SERVICE_ID,
